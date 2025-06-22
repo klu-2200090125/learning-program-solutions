40f9fa1a5ff623af5aac9c6d6cb4fa5d28ec7ac8
klu-2200090125 <153178870+klu-2200090125@users.noreply.github.com>
2025-06-22 18:34:38 +0530

    Add files via upload

---
+++ Week 2 Design Patterns and Principles/Exercise_2_FactoryMethod/PdfDocumentFactory.java
@@ -0,0 +1,7 @@
+package Week1.FactoryMethodPatternExample;
+
+public class PdfDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new PdfDocument();
+    }
+}
---
+++ Week 2 Design Patterns and Principles/Exercise_2_FactoryMethod/WordDocument.java
@@ -0,0 +1,7 @@
+package Week1.FactoryMethodPatternExample;
+
+public class WordDocument implements Document {
+    public void open() {
+        System.out.println("Opening Word Document.");
+    }
+}
